60022180c887befa32f8992de1e642321a271fbb
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-06-09 10:01:29 +0300

    card project

--- my-resume/src/components/ProjectsPage/ProjectsPage.tsx
+++ my-resume/src/components/ProjectsPage/ProjectsPage.tsx
@@ -8,11 +8,53 @@ export const ProjectsPage: FC = () => {
 
   return (
     <div className={styles.projects__wrapper}>
-      <h2 className={styles["projects__wrapper__title"]}>
+      <h2 className={styles["projects__wrapper-title"]}>
         {t("menu_projects")}
       </h2>
 
       <ul className={styles["projects__card-list"]}>
+        <li className={styles["projects__card-item"]}>
+          <img
+            className={styles["projects__card-image"]}
+            src="/images/studio.png"
+            alt="Изображение сайта подкастов"
+          />
+          <div className={styles["projects__card-wrapper"]}>
+            <div className={styles["projects__card-top"]}>
+              <h3 className={styles["projects__card-title"]}>
+                {t("projects__card-title")}
+              </h3>
+              <p className={styles["projects__card-text"]}>
+                {t("projects__card-text1")}
+              </p>
+              <span className={styles["projects__card-stack"]}>
+                Tech stack : HTML , JavaScript,TypeScript, SASS
+              </span>
+            </div>
+            <div className={styles["projects__card-links"]}>
+              <a
+                href="https://irinapodlisnyuk.github.io/VibeCast-Studio/"
+                className={styles["projects__link"]}
+              >
+                <Icon
+                  name="preview"
+                  className={styles["projects__link-icon"]}
+                />
+                <span className={styles["projects__link-text"]}>
+                  Live Preview
+                </span>
+              </a>
+
+              <a
+                href="https://github.com/irinapodlisnyuk/VibeCast-Studio"
+                className={styles["projects__link"]}
+              >
+                <Icon name="git" className={styles["projects__link-icon"]} />
+                <span className={styles["projects__link-text"]}>View Code</span>
+              </a>
+            </div>
+          </div>
+        </li>
         <li className={styles["projects__card-item"]}>
           <img
             className={styles["projects__card-image"]}
@@ -30,19 +72,44 @@ export const ProjectsPage: FC = () => {
           </div>
           <div className={styles["projects__card-links"]}>
             <a href="#" className={styles["projects__link"]}>
-              <Icon
-                name="preview"
-                className={styles["projects__link-icon"]}
-              />
-              <span  className={styles["projects__link-text"]}>Live Preview</span>
+              <Icon name="preview" className={styles["projects__link-icon"]} />
+              <span className={styles["projects__link-text"]}>
+                Live Preview
+              </span>
             </a>
 
-             <a href="#" className={styles["projects__card-link"]}>
-              <Icon
-                name="git"
-                className={styles["projects__card-icon"]}
-              />
-              <span  className={styles["projects__link-text"]}>View Code</span>
+            <a href="#" className={styles["projects__link"]}>
+              <Icon name="git" className={styles["projects__link-icon"]} />
+              <span className={styles["projects__link-text"]}>View Code</span>
+            </a>
+          </div>
+        </li>
+        <li className={styles["projects__card-item"]}>
+          <img
+            className={styles["projects__card-image"]}
+            src="/images/studio.png"
+            alt="Изображение сайта подкастов"
+          />
+
+          <div className={styles["projects__card-top"]}>
+            <h3 className={styles["projects__card-title"]}>
+              {t("projects__card-title")}
+            </h3>
+            <p className={styles["projects__card-text"]}>
+              {t("projects__card-text1")}
+            </p>
+          </div>
+          <div className={styles["projects__card-links"]}>
+            <a href="#" className={styles["projects__link"]}>
+              <Icon name="preview" className={styles["projects__link-icon"]} />
+              <span className={styles["projects__link-text"]}>
+                Live Preview
+              </span>
+            </a>
+
+            <a href="#" className={styles["projects__link"]}>
+              <Icon name="git" className={styles["projects__link-icon"]} />
+              <span className={styles["projects__link-text"]}>View Code</span>
             </a>
           </div>
         </li>
