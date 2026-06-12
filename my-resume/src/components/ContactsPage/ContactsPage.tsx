55807413825dc61535473f8f06f693de2f3cbf99
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-06-12 14:13:05 +0300

    form comtact

--- my-resume/src/components/ContactsPage/ContactsPage.tsx
+++ my-resume/src/components/ContactsPage/ContactsPage.tsx
@@ -1,20 +1,46 @@
 import { useTranslation } from "react-i18next";
 import styles from "./ContactsPage.module.scss";
+import Icon from "../types/Icon";
 
 export const ContactsPage = () => {
   const { t } = useTranslation();
   return (
     <div className={styles["contacts__wrapper"]}>
-      <h2 className={styles["contacts__wrapper__title"]}>
-        {t("contacts__title")}
-      </h2>
-
+      <img
+        className={styles["contacts__image"]}
+        src="/images/working.png"
+        alt="girl image"
+      />
       <div className={styles["contacts__info"]}>
+        <h3 className={styles["contacts__info-title"]}>
+          {t("contacts__title")}
+        </h3>
+        <p className={styles["contacts__info-text"]}>{t("contacts__text")}</p>
+        <div className={styles["contacts__contact"]}>
+          <a
+            href="tel:+79991234567"
+            className={styles["contacts__contact-tel"]}
+          >
+            <Icon name="phone" className={styles["contacts__contact-icon"]} />
+            <span className={styles["contacts__contact-text"]}>+7 (993)-311-38-48</span>
+          </a>
 
+          <a
+            href="mailto:irinapodlisnyuk@gmail.com"
+            className={styles["contacts__contact-email"]}
+          >
+            <Icon name="email" className={styles["contacts__contact-icon"]} />
+            <span className={styles["contacts__contact-text"]}>irinapodlisnyuk@gmail.com</span>
+          </a>
+        </div>
       </div>
 
-      <div className={styles["contacts__forma"]}>
+      <div className={styles["contacts__form"]}>
+         <h3 className={styles["contacts__form-title"]}>
+          {t("contacts__form-title")}
+        </h3>
 
+        
       </div>
     </div>
   );
