fa7582aed3dae917d1fb86c8ae33e4024b36e645
irinapodlisnyuk <irinapodlisnyuk@gmail.com>
2026-06-13 16:59:07 +0300

    Добавлен скрипт автодеплоя Actions

--- my-resume/vite.config.js
+++ my-resume/vite.config.js
@@ -9,6 +9,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/My_resume/", 
+
   css: {
     preprocessorOptions: {
       scss: {
@@ -31,5 +33,44 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  
+   build: {
+    emptyOutDir: true 
+  }
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { fileURLToPath } from "url";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         silenceDeprecations: ["import"],
+//         additionalData: `@use "@/assets/styles/global/_variables.scss" as *;
+//            @use "@/assets/styles/global/_mixins.scss" as *;
+//            @use "@/assets/styles/global/_btn.scss" as *;`,
+//       },
+//     },
+//   },
+//   plugins: [
+//     tailwindcss(),  
+//     react(),
+//   ],
+//   server: {
+//     open: true,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+  
+// });
